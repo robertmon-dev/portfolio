78bb2006d30409075bcde30cec84d860e145c120
Robert Moń <108728678+robertmon-dev@users.noreply.github.com>
2026-03-27 22:50:08 +0100

    fix(trpcClient): unify toasting

--- client/src/lib/trpc/queryClient.ts
+++ client/src/lib/trpc/queryClient.ts
@@ -1,22 +1,12 @@
 import { QueryClient, MutationCache, QueryCache } from '@tanstack/react-query';
 import { TRPCClientError } from '@trpc/client';
-import { handleTrpcError } from './handlers/trpcError';
+import { notifyError } from './handlers/trpcError';
 import { toast } from 'react-toastify';
 
 export const queryClient = new QueryClient({
   mutationCache: new MutationCache({
-    onError: (error) => {
-      const message = handleTrpcError(error);
-      if (message) {
-        toast.error(message, {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-        });
-      }
+    onError: (err) => {
+      notifyError(err);
     },
   }),
 
