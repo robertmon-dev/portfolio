5a12428f5ac90e5d48a8f2e3c073e975d1f2936f
Robert Moń <m0ni3v@gmail.com>
2026-04-04 15:55:33 +0200

    chore(switch): standardize import and string quotes
    The import statements and string quotes in the Switch component were updated to maintain consistent use of double quotes as per the project's coding standards. This change ensures uniformity and better readability throughout the codebase.

--- client/src/components/atoms/Switch/Switch.tsx
+++ client/src/components/atoms/Switch/Switch.tsx
@@ -1,58 +1,56 @@
-import { forwardRef, useId } from 'react';
-import type { SwitchProps, CustomCSS } from './types';
-import { getSwitchClasses } from './styles';
-import './Switch.scss';
+import { forwardRef, useId } from "react";
+import type { SwitchProps, CustomCSS } from "./types";
+import { getSwitchClasses } from "./styles";
+import "./Switch.scss";
 
-export const Switch = forwardRef<HTMLInputElement, SwitchProps>((props, ref) => {
-  const {
-    size = 'md',
-    label,
-    iconOff,
-    iconOn,
-    colorOn,
-    colorOff,
-    className,
-    disabled,
-    style,
-    ...rest
-  } = props;
+export const Switch = forwardRef<HTMLInputElement, SwitchProps>(
+  (props, ref) => {
+    const {
+      size = "md",
+      label,
+      iconOff,
+      iconOn,
+      colorOn,
+      colorOff,
+      className,
+      disabled,
+      style,
+      ...rest
+    } = props;
 
-  const uniqueId = useId();
-  const id = props.id || uniqueId;
-  const classes = getSwitchClasses({ size, disabled, className });
-  const dynamicStyles: CustomCSS = {
-    ...style,
-    '--switch-color-on': colorOn,
-    '--switch-color-off': colorOff,
-  };
+    const uniqueId = useId();
+    const id = props.id || uniqueId;
+    const classes = getSwitchClasses({ size, disabled, className });
+    const dynamicStyles: CustomCSS = {
+      ...style,
+      "--switch-color-on": colorOn,
+      "--switch-color-off": colorOff,
+    };
 
-  return (
-    <label htmlFor={id} className={classes} style={dynamicStyles}>
-      <input
-        ref={ref}
-        type="checkbox"
-        id={id}
-        disabled={disabled}
-        role="switch"
-        {...rest}
-      />
+    return (
+      <label htmlFor={id} className={classes} style={dynamicStyles}>
+        <input
+          ref={ref}
+          type="checkbox"
+          id={id}
+          disabled={disabled}
+          role="switch"
+          {...rest}
+        />
 
-      <span className="switch-slider" aria-hidden="true">
-        {iconOff && (
-          <span className="switch-icon switch-icon--off">
-            {iconOff}
-          </span>
-        )}
+        <span className="switch-slider" aria-hidden="true">
+          {iconOff && (
+            <span className="switch-icon switch-icon--off">{iconOff}</span>
+          )}
 
-        {iconOn && (
-          <span className="switch-icon switch-icon--on">
-            {iconOn}
-          </span>
-        )}
-      </span>
-      {label && <span className="switch-label">{label}</span>}
-    </label>
-  );
-});
+          {iconOn && (
+            <span className="switch-icon switch-icon--on">{iconOn}</span>
+          )}
+        </span>
+        {label && <span className="switch-label">{label}</span>}
+      </label>
+    );
+  },
+);
 
-Switch.displayName = 'Switch';
+Switch.displayName = "Switch";
