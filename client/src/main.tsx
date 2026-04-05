d159b88d32853745d7fa3ea72dc56c7362768c0d
Robert Moń <m0ni3v@gmail.com>
2026-04-05 17:52:03 +0200

    style(client): replace single quotes with double quotes
    This change updates the code style in the main.tsx file of the client. Single quotes have been replaced with double quotes to maintain consistency with the project's coding standards.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { TRPCProvider } from './lib/trpc/Provider'
-import App from './App'
-import './i18n'
-import './index.scss'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { TRPCProvider } from "./lib/trpc/Provider";
+import App from "./App";
+import "./i18n";
+import "./index.scss";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <TRPCProvider>
       <App />
     </TRPCProvider>
   </StrictMode>,
-)
+);
