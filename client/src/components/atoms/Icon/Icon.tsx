6bd800e7fa56453a2391eb0022ac7e7de55d30e3
Robert Moń <m0ni3v@gmail.com>
2026-04-05 17:43:40 +0200

    feat(icon): optimize TechIcon rendering
    Enhanced TechIcon with memoization and displayName. Utilized React's `memo` to prevent unnecessary re-renders and `useMemo` for efficient icon retrieval. This improves the rendering performance of the Icon component, particularly for dynamic or frequently changing props.

--- client/src/components/atoms/Icon/Icon.tsx
+++ client/src/components/atoms/Icon/Icon.tsx
@@ -1,13 +1,16 @@
+import { memo, useMemo } from "react";
 import type { TechIconProps } from "./types";
 import { getTechIcon } from "@/lib/utils/render";
 import "./Icon.scss";
 
-export const TechIcon = ({ name, color, size = 20 }: TechIconProps) => {
-  const IconComponent = getTechIcon(name);
+export const TechIcon = memo(({ name, color, size = 20 }: TechIconProps) => {
+  const IconComponent = useMemo(() => getTechIcon(name), [name]);
 
   return (
     <div className="tech-icon" style={{ color: color || "inherit" }}>
       <IconComponent size={size} />
     </div>
   );
-};
+});
+
+TechIcon.displayName = "TechIcon";
