1903f3e32ea8f646c903a3d2a7b4c5139e989fa3
Robert Moń <m0ni3v@gmail.com>
2026-04-19 12:40:19 +0200

    test(app/github): solve proper naming for files

--- app/src/services/__test__/github/Get.test.ts
+++ app/src/services/__test__/github/Get.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from "vitest";
 import { useServiceTest } from "../../../mocks/core";
-import { GetGithubStatsService } from "../../../services/github/Get";
+import { GetGithubStatsService } from "../../github/Get";
 import { createFakeGithubStats } from "./utils";
-import { githubStatsWithDeepRelationsQuery } from "../../../services/github/queries";
+import { githubStatsWithDeepRelationsQuery } from "../../github/queries";
 import { GithubStatsSchema } from "@portfolio/shared";
 
 describe("GetGithubStatsService", async () => {
@@ -12,15 +12,17 @@ describe("GetGithubStatsService", async () => {
     const persistedStats = createFakeGithubStats(1)[0];
 
     ctx.mocks.prisma.githubStats.findUnique.mockResolvedValue(persistedStats);
-    expect(ctx.mocks.cache.get).toHaveBeenCalledWith(
+    const result = await ctx.service.execute(ctx.mocks.settings.NICKNAME);
+
+    expect(ctx.mocks.cache.wrap).toHaveBeenCalledWith(
       `github:stats:${ctx.mocks.settings.NICKNAME}`,
+      3600,
+      expect.any(Function),
     );
     expect(ctx.mocks.prisma.githubStats.findUnique).toHaveBeenCalledWith({
       where: { username: ctx.mocks.settings.NICKNAME },
       ...githubStatsWithDeepRelationsQuery,
     });
-
-    const result = await ctx.service.execute(ctx.mocks.settings.NICKNAME);
 
     const parsed = GithubStatsSchema.parse(result);
     expect(parsed).toBeTypeOf(typeof GithubStatsSchema);
