4254433dd7d550ac51377f91728f0b64d9d3ba2e
Robert Moń <m0ni3v@gmail.com>
2026-05-10 08:37:08 +0200

    feat(routers): add category router with getById and list procedures
    Introduced a new category router to handle requests related to tag categories. The router includes two procedures: getById to fetch a single category by its ID and list to retrieve a paginated list of categories. Both procedures initially attempt to access cached data before querying the database if necessary. This implementation enhances the API with endpoints to manage tag categories more efficiently.

---
+++ app/src/routers/public/category.ts
@@ -0,0 +1,52 @@
+import z from "zod";
+import {
+  ListTagCategoriesInputSchema,
+  ListTagCategoriesOutputSchema,
+} from "@portfolio/shared";
+import { router } from "../../trpc/init";
+import { publicProcedure } from "../../trpc/procedures/public";
+import { zUuid, TagCategorySchema } from "@portfolio/shared";
+import {
+  executeAuthorizedService,
+  executeService,
+} from "src/trpc/executers/base";
+import { GetTagCategoriesService } from "src/services/tagCategories/Get";
+import { ListTagCategoriesService } from "src/services/tagCategories/List";
+
+export const categoryRouter = router({
+  getById: publicProcedure
+    .meta({
+      openapi: {
+        method: "GET",
+        path: "/categories/{id}",
+        tags: ["Categories"],
+        summary: "Fetches single tag category by ID",
+        description:
+          "Returns an tag category with it's relation via provided identifier, firstly hits cache, if not present check for persited data",
+        protect: false,
+      },
+    })
+    .input(z.object({ id: zUuid }))
+    .output(TagCategorySchema)
+    .query(async ({ ctx, input }) => {
+      return executeService(GetTagCategoriesService, ctx, input.id);
+    }),
+
+  list: publicProcedure
+    .meta({
+      openapi: {
+        method: "GET",
+        path: "/categories/",
+        tags: ["Categories"],
+        summary: "Fetches list of tag categories",
+        description:
+          "Returns paginated list of tag categories. At the very first hits cache, if not present we fallback to database checking for present records",
+        protect: false,
+      },
+    })
+    .input(ListTagCategoriesInputSchema)
+    .output(ListTagCategoriesOutputSchema)
+    .query(async ({ ctx, input }) => {
+      return executeService(ListTagCategoriesService, ctx, input);
+    }),
+});
