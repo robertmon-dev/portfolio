3b3ef2225829a9627978e3ee90bc7e5f38f1ae4e
Robert Moń <m0ni3v@gmail.com>
2026-07-03 15:39:15 +0200

    fix(Blog): correct initial state variable name in useCommentsState
    Corrected the usage of an initial state variable from `initialState` to `commentsInitialState` to ensure the correct state is initialized in the `useCommentsState` hook.

--- client/src/pages/Blog/useCommentsState.ts
+++ client/src/pages/Blog/useCommentsState.ts
@@ -1,10 +1,10 @@
 import { useReducer } from "react";
-import { commentReducer, initialState } from "./types";
+import { commentReducer, commentsInitialState } from "./types";
 import { useCommentMutations } from "./useBlogMutations";
 import { useCommentActions } from "./useCommentsActions";
 
 export const useCommentsState = () => {
-  const [state, dispatch] = useReducer(commentReducer, initialState);
+  const [state, dispatch] = useReducer(commentReducer, commentsInitialState);
 
   const mutations = useCommentMutations();
   const actions = useCommentActions(mutations, dispatch);
